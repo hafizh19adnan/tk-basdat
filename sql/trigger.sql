0f019558fbd89d9ff03173b00a2c45b1a83c1629
hafizh19adnan <hafizh19adnan@gmail.com>
2017-05-01 23:29:20 +0700

    Add new html page

---
+++ sql/trigger.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE FUNCTION shiplist_proc()
+RETURNS trigger AS
+$$
+BEGIN
+IF (TG_OP = 'INSERT') THEN
+	UPDATE SHIPPED_PRODUK SET stok=stok+1 WHERE kode_produk = NEW.kode_produk;
+ELSIF (TG_OP='DELETE') THEN
+	UPDATE SHIPPED_PRODUK SET stok=stok-1 WHERE kode_produk = OLD.kode_produk;
+ELSIF (TG_OP='UPDATE') THEN
+	UPDATE SHIPPED_PRODUK SET stok=stok+(NEW.stok-OLD.stok) WHERE kode_produk=NEW.kode_produk;
+END IF;
+RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER shiplist_trigger
+AFTER INSERT OR DELETE OR UPDATE
+ON LIST_ITEM FOR EACH ROW
+EXECUTE PROCEDURE shiplist_proc();
+
+CREATE OR REPLACE FUNCTION poin_proc()
+RETURNS trigger AS
+$$
+BEGIN
+IF (TG_OP = 'UPDATE') THEN
+	IF(NEW.status='4') THEN
+		UPDATE PELANGGAN SET poin=poin+(0.01*NEW.total_bayar) WHERE email = NEW.email_pembeli;
+	END IF;
+END IF;
+RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER poin_trigger
+AFTER UPDATE
+ON TRANSAKSI_SHIPPED FOR EACH ROW
+EXECUTE PROCEDURE poin_proc();
